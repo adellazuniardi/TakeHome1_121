69867d95f54076f1d8ca027f6810ccc299135e31
adellazuniardi <96398704+adellazuniardi@users.noreply.github.com>
2022-10-30 21:31:36 +0700

    step1-build project

---
+++ src/main/java/Project/TokoMainan/TokoMainanApplication.java
@@ -0,0 +1,13 @@
+package Project.TokoMainan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokoMainanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokoMainanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Project/TokoMainan/TokoMainanApplicationTests.java
@@ -0,0 +1,13 @@
+package Project.TokoMainan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokoMainanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
